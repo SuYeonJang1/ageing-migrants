b16cd35d0498130135a3ee7c2d8d318968cbfb3a
SuYeonJang1 <153096703+SuYeonJang1@users.noreply.github.com>
2023-12-06 18:51:34 +0100

    Update 8-sensitivity_cvd_diabetes.R

--- supplements/8-sensitivity_cvd_diabetes.R
+++ supplements/8-sensitivity_cvd_diabetes.R
@@ -2,12 +2,6 @@
 # packages ----------------------------------------------------------------
 
 library(plm)
-library(RColorBrewer)
-library(ggplot2)
-library(extrafont)
-library(grid)
-library(gridExtra)
-library(gtable)
 
 
 # bring data --------------------------------------------------------------
@@ -65,56 +59,4 @@ mfe1 <- plm(ndisease ~ agegr + imm, data = p2, model = "within")
 mfe2 <- plm(ndisease ~ agegr * imm, data = p2, model = "within")
 mfe3 <- plm(ndisease ~ agegr * imm + income + employed + marry, 
             data = p2, model = "within")
-
-
-# prediction --------------------------------------------------------------
-
-source("function/pfitted.R")
-source("function/pdiff.R")
-
-lab <- c(paste0(seq(50,75,5),"-",seq(54,79,5)))
-
-l1 <- levels(p1$agegr)
-l2 <- levels(p1$imm)
-
-pred_ols <- pfitted(mols3, at = list(agegr = l1, imm = l2))
-pred_ols$imm <- as.character(pred_ols$imm)
-pred_ols$agegr <- as.numeric(pred_ols$agegr)
-
-pred_fe <- pfitted(mfe3, at = list(agegr = l1, imm = l2))
-pred_fe$imm <- as.character(pred_fe$imm)
-pred_fe$agegr <- as.numeric(pred_fe$agegr)
-
-pred_re <- pfitted(mre3, at = list(agegr = l1, imm = l2))
-pred_re$imm <- as.character(pred_re$imm)
-pred_re$agegr <- as.numeric(pred_re$agegr)
-
-pred_ols
-
-pred_re
-
-pred_fe
-
-
-# differential ------------------------------------------------------------
-
-diff_ols <- pdiff(mols3, at = list(agegr = l1, imm = l2), by = list(imm = "0"))
-diff_ols <- diff_ols[diff_ols$imm == "1",]
-row.names(diff_ols) <- NULL
-
-diff_fe <- pdiff(mfe3, at = list(agegr = l1, imm = l2), by = list(imm = "0"))
-diff_fe <- diff_fe[diff_fe$imm == "1" & diff_fe$agegr != l1[1],]
-row.names(diff_fe) <- NULL
-
-diff_re <- pdiff(mre3, at = list(agegr = l1, imm = l2), by = list(imm = "0"))
-diff_re <- diff_re[diff_re$imm == "1",]
-row.names(diff_re) <- NULL
-
-diff_ols
-
-diff_re
-
-diff_fe
-
-
 
